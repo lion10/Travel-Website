6798d346af7f7299e9dcd3095e8a755aa39ffb55
Omar Abusabha <ssomar2028@gmail.com>
2020-09-02 16:34:47 +0300

    Display Product Lists In the Landing Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { FaCode } from "react-icons/fa";
 import Axios from "axios";
-import { response } from "express";
+import { Col, Card, Row } from "antd";
+import Icon from "@ant-design/icons";
+import ImageSlider from "../../utils/ImageSlider";
+const { Meta } = Card;
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -10,14 +12,54 @@ function LandingPage() {
     Axios.post("/api/product/getProducts").then((response) => {
       if (response.data.success) {
         setProducts(response.data.products);
-        console.log(response.data.products);
       } else {
         alert("Failed to fetch products data");
       }
     });
   }, []);
 
-  return <></>;
+  const renderCards = Products.map((product, index) => {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <Card
+          hoverable={true}
+          cover={<ImageSlider images={product.images}></ImageSlider>}
+        >
+          <Meta title={product.title} description={`$${product.price}`}></Meta>
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />
+        </h2>
+      </div>
+
+      {/* {Filter} */}
+      {/* {Search} */}
+
+      {Products.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            height: "300px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h2>No post yet...</h2>
+        </div>
+      ) : (
+        <div>
+          <Row gutter={[16, 16]}>{renderCards}</Row>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default LandingPage;
